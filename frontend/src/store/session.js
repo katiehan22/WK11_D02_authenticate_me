22053705d17b1ca405d8cb49f8a08a915c999098
katiehan22 <katiehan22@gmail.com>
2022-10-26 15:34:43 -0700

    Add logout thunk action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -71,6 +71,15 @@ export const signup = (user) => async (dispatch) => {
   return res;
 }
 
+export const logout = () => async (dispatch) => {
+  const res = await csrfFetch('/api/session', {
+    method: 'DELETE'
+  });
+  storeCurrentUser(null);
+  dispatch(removeCurrentUser());
+  return res;
+};
+
 const initialState = { user: JSON.parse(sessionStorage.getItem("currentUser")) };
 const sessionReducer = (state = initialState, action) => {
   switch (action.type) {
